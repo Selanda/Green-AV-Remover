ab261ba1151ecc9f391f1948c1aa47493278b536
Selanda <64166375+Selanda@users.noreply.github.com>
2020-05-18 22:21:23 -0400

    Removed QBF statements

--- Green-AV Remover.bat
+++ Green-AV Remover.bat
@@ -1,19 +1,3 @@
-REM  QBFC Project Options Begin
-REM  HasVersionInfo: Yes
-REM  Companyname: TheHelpfulHacker
-REM  Productname: Green-AV Remover
-REM  Filedescription: 
-REM  Copyrights: Creative Commons
-REM  Trademarks: Creative Commons
-REM  Originalname: 
-REM  Comments: Do not reproduce, if you obtained this file from anywhere other than TheHelpfulHacker.com it isn't genuine.
-REM  Productversion: 01.06.06.06
-REM  Fileversion: 01.02.00.00
-REM  Internalname: 
-REM  Appicon: F:\My Pictures\smile.ico
-REM  Embeddedfile: C:\Users\Selanda\Desktop\Disassembly\Badware Samples (pw - infected)\Green-AV\Remover\keydel.reg
-REM  QBFC Project Options End
-
 @echo off
 
 echo This application removes the malware identified as Green-AV Fake Antivirus.
@@ -37,8 +21,6 @@ regedit /s %MYFILES%\keydel.reg
 
 echo.
 echo The system will automatically restart to complete the removal.
-echo For problems, questions, comments or suggestions please contact:
-echo Selanda _at_ thehelpfulhacker _dot_ com
 echo.
 
 pause
